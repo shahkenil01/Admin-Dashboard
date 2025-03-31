596f02a93c141901c1b6237485bf5d1c86f54bf4
shahkenil01 <shahkenil430@gmail.com>
2025-03-31 18:20:03 +0530

    Quick update

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,18 +1,25 @@
 import { Link } from 'react-router-dom';
 import logo from '../../assets/images/logo.png';
+import Button from '@mui/material/Button';
+import { MdMenuOpen } from "react-icons/md";
+import { MdOutlineMenu } from "react-icons/md";
 
 const Header = () => {
   return (
     <>
       <header className="d-flex align-items-center">
         <div className="container-fluid w-100">
-          <div className="row d-flex align-items-center">
+          <div className="row d-flex align-items-center w-100">
             {/* Logo Wraper */}
-            <div className="col-xs-3">
+            <div className="col-sm-2 part1">
               <Link to={'/'} className='d-flex align-items-center logo'>
                 <img src={logo}/>
                 <span className='ml-2'>HOTASH</span>
               </Link>
+            </div>
+
+            <div className='col-sm-3 d-flex align-items-center part2'>
+              <Button className='rounded-circle'><MdMenuOpen/></Button>
             </div>
           </div>
         </div>
