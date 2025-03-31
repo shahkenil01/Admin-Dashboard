3f484e82289aea1daab85c0ff42c510ed6d04bc2
shahkenil01 <shahkenil430@gmail.com>
2025-03-31 17:48:23 +0530

    Quick update

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,12 +1,24 @@
+import { Link } from 'react-router-dom';
+import logo from '../../assets/images/logo.png';
 
-const Header = ( ) => {
-  return(
+const Header = () => {
+  return (
     <>
-      <header>
-        
+      <header className="d-flex align-items-center">
+        <div className="container-fluid w-100">
+          <div className="row d-flex align-items-center">
+            {/* Logo Wraper */}
+            <div className="col-xs-3">
+              <Link to={'/'} className='d-flex align-items-center logo'>
+                <img src={logo}/>
+                <span className='ml-2'>HOTASH</span>
+              </Link>
+            </div>
+          </div>
+        </div>
       </header>
     </>
-  )
-}
+  );
+};
 
 export default Header;
