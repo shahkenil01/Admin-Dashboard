4615e38f6a1cab8cb150ee615e5224d971df1c8d
shahkenil01 <shahkenil430@gmail.com>
2025-04-01 13:09:52 +0530

    Quick update

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -8,21 +8,18 @@ import SearchBox from '../SearchBox';
 import { MdOutlineLightMode } from 'react-icons/md';
 import { FaRegBell } from 'react-icons/fa6';
 
-import { Menu, MenuItem, Avatar, Divider, ListItemIcon } from "@mui/material";
-import { Logout, Settings, PersonAdd, AccountCircle } from "@mui/icons-material";
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import Tooltip from '@mui/material/Tooltip';
+import { Menu, MenuItem, ListItemIcon } from "@mui/material";
+import { Logout, Settings, PersonAdd } from "@mui/icons-material";
 
 const Header = () => {
   const [anchorEl, setAnchorEl] = 
   useState(null);
   const open = Boolean(anchorEl);
 
-  const handleClick = (event) => {
+  const handleOpenMyAccDrop = (event) => {
     setAnchorEl(event.currentTarget);
   };
-  const handleClose = () => {
+  const handleCloseMyAccDrop = () => {
     setAnchorEl(null);
   };
 
@@ -46,50 +43,12 @@ const Header = () => {
             </div>
 
             <div className="col-sm-7 d-flex align-items-center justify-content-end part3">
+
               <Button className="rounded-circle mr-3"> <MdOutlineLightMode /> </Button>
-              <Button className="rounded-circle mr-3" onClick={handleClick}> <FaRegBell /> </Button>
-                <Menu
-                  anchorEl={anchorEl}
-                  id="account-menu"
-                  open={open}
-                  onClose={handleClose}
-                  onClick={handleClose}
-                  slotProps={{
-                    paper: {
-                      elevation: 0,
-                      sx: {
-                        overflow: 'visible', filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))', mt: 1.5,
-                        '& .MuiAvatar-root': {
-                          width: 32, height: 32, ml: -0.5, mr: 1,
-                        },
-                        '&::before': {
-                          content: '""', display: 'block', position: 'absolute', top: 0, right: 14, width: 10, height: 10, bgcolor: 'background.paper', transform: 'translateY(-50%) rotate(45deg)', zIndex: 0,
-                        },
-                      },
-                    },
-                  }}
-                  transformOrigin={{ horizontal: 'right', vertical: 'top' }}
-                  anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
-                >
-                  <MenuItem onClick={handleClose}> <Avatar /> Profile </MenuItem>
-                  <MenuItem onClick={handleClose}> <Avatar /> My account </MenuItem>
-                  <Divider />
-                  <MenuItem onClick={handleClose}>
-                    <ListItemIcon> <PersonAdd fontSize="small" /> </ListItemIcon>
-                    Add another account
-                  </MenuItem>
-                  <MenuItem onClick={handleClose}>
-                    <ListItemIcon> <Settings fontSize="small" /> </ListItemIcon>
-                    Settings
-                  </MenuItem>
-                  <MenuItem onClick={handleClose}>
-                    <ListItemIcon> <Logout fontSize="small" /> </ListItemIcon>
-                    Logout
-                  </MenuItem>
-                </Menu>
+              <Button className="rounded-circle mr-3"> <FaRegBell /> </Button>
 
               <div className="myAccWrapper">
-                <Button className="myAcc d-flex align-items-center">
+                <Button className="myAcc d-flex align-items-center" onClick={handleOpenMyAccDrop}>
                   <div className="userImg">
                     <span className="rounded-circle">
                       <img src="https://mironcoder-hotash.netlify.app/images/avatar/01.webp" alt="profile"/>
@@ -101,6 +60,30 @@ const Header = () => {
                     <p className="mb-0">@Kenil</p>
                   </div>
                 </Button>
+
+                <Menu
+                  anchorEl={anchorEl}
+                  id="account-menu"
+                  open={open}
+                  onClose={handleCloseMyAccDrop}
+                  onClick={handleCloseMyAccDrop}
+                  transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+                  anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+                >
+                  <MenuItem onClick={handleCloseMyAccDrop}>
+                    <ListItemIcon> <PersonAdd fontSize="small" /> </ListItemIcon>
+                    My Account
+                  </MenuItem>
+                  <MenuItem onClick={handleCloseMyAccDrop}>
+                    <ListItemIcon> <Settings fontSize="small" /> </ListItemIcon>
+                    Reset Password
+                  </MenuItem>
+                  <MenuItem onClick={handleCloseMyAccDrop}>
+                    <ListItemIcon> <Logout fontSize="small" /> </ListItemIcon>
+                    Logout
+                  </MenuItem>
+                </Menu>
+
               </div>
             </div>
           </div>
