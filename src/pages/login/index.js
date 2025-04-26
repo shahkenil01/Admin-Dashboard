90803e579089f5d48fb5ca56d303f70e8a64193b
shahkenil01 <shahkenil430@gmail.com>
2025-04-26 21:31:06 +0530

    Quick update

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,4 +1,4 @@
-import { useEffect, useContext } from 'react';
+import { useEffect, useContext, useState } from 'react';
 import Logo from '../../assets/images/logo.png';
 import { MyContext } from '../../App';
 import pattern from '../../assets/images/pattern.webp';
@@ -7,23 +7,32 @@ import { RiLockPasswordFill } from "react-icons/ri";
 
 const Login = ()=>{
 
-  const context = useContext(MyContext)
+  const [inputIndex, setInputIndex] = useState(null);
+  const context = useContext(MyContext);
+
+  const focusInput=(index)=>{
+    setInputIndex(index);
+  }
 
   return(
     <>
-      <img src={pattern} className='loginPatern'/>
+      <img src={pattern} className='loginPatern' alt="Pattern"/>
       <section className="loginSection">
         <div className="loginBox">
           <div className='logo text-center'>
-            <img src={Logo} width="60px"/>
+            <img src={Logo} width="60px" alt="Logo"/>
             <h5 className='font-weight-bold'>Login to Hotash</h5>
           </div>
 
           <div className='wrapper mt-3 card border p-4'>
             <form>
-              <div className='form-group mb-3 position-relative'>
+              <div className={`form-group mb-3 position-relative ${inputIndex===0 && 'focus'}`}>
                 <span className='icon'><MdEmail/></span>
-                <input type='text' className='form-control' placeholder='enter your email'></input>
+                <input type='email'
+                  className='form-control' 
+                  placeholder='enter your email' 
+                  onFocus={()=>focusInput(0)}
+                  onBlur={()=>setInputIndex(null)}></input>
               </div>
             </form>
           </div>
